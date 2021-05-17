3ad89b6d0b49642cbed0f01d189f87bc50176331
MANISH SINGH <manishsingh332000@gmail.com>
2021-05-17 13:11:30 +0530

    Project setup

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const http = require('http');
 const port = 8000;  
+
+const server = http.createServer();
+
+server.listen(port,function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("SERVER IS UP AND RUNNIG ON PORT: ", port);
+});
