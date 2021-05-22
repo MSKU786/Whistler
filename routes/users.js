0a7771760e7c8d5964c654b32cb073dd0b01479e
MANISH SINGH <manishsingh332000@gmail.com>
2021-05-22 15:56:45 +0530

    Update user complete

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,58 @@
 const express = require('express');
-
 const router = express.Router();
+const bcrypt = require("bcrypt");
 //const homeController = require('../controllers/home_controller');
 
 //router.get('/',homeController.home);
+//Update User
+router.put("/:id", async (req, res) =>{
+    if(req.body.userId === req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            }
+            catch(err){
+                 return res.json(err);
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            });
+            res.status(200).json("Account updated");
+        }
+    }
+    else{
+        return res.status(403).json("You can update only your account!");
+    }
+})
+
+//Delete User
+router.put("/:id", async (req, res) =>{
+    if(req.body.userId === req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            }
+            catch(err){
+                 return res.json(err);
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            });
+            res.status(200).json("Account updated");
+        }
+    }
+    else{
+        return res.status(403).json("You can update only your account!");
+    }
+})
+
+
 router.get('/', (req, res) => {
     res.send("Hye its router use");
 })
