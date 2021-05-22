b92e959209ad34d885e1e609e04d918a8973318b
MANISH SINGH <manishsingh332000@gmail.com>
2021-05-22 06:32:02 +0530

    Server connected

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express();
 const port = 8000;  
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const usersRoute = require("./routes/users")
+dotenv.config();
 
-
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, ()=>{
+     console.log("connected to mongo");
+});
 //Use express router
-app.use('/', require())
+//app.use('/', require())
+//MiddleWare
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgon("Common"));
+
+app.use('/api/users', usersRoute);
+
+
 app.listen(port,function(err){
     if(err){
         console.log(err);
