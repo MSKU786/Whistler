528f878279a684f3627ae440bca820b660002146
MANISH SINGH <manishsingh332000@gmail.com>
2021-05-22 19:28:10 +0530

    Followers added

--- routes/users.js
+++ routes/users.js
@@ -54,12 +54,41 @@ router.delete("/:id", async (req, res) =>{
 //Get a user
 router.get("/:id", async (req, res) =>{
     try{
-        const user = User.findById(req.params.id);
-        res.status("success").json(user);
+        
+        
+        const user = await User.findById(req.params.id);
+        const {password, updatedAt, ...other} = user._doc 
+        res.status(200).json(other);
     }
     catch(err){
         res.status(500).json(err);
     }
 });
 
+
+//Follow a user
+router.put('/:id/follow', async (req,res) =>{
+    if(req.body.userID !== req.params.id)
+    {
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userID);
+            if(!user.followers.includes(req.body.userID))
+            {
+                await user.updateOne({$push: { followers: req.body.userID }});
+                await currentUser.updateOne({$push: { following: req.params.id }});
+                res.status(200).json("User has been followed");
+            }
+            else{
+                res.status(403).json("You already follow this user");
+            }
+        }catch(err)
+        {
+            res.status(500).json(err);
+        }
+    }
+    else{
+        res.status(403).json("You can't follow yourself idiot");
+    }
+})
 module.exports = router;
