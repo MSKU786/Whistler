64c3e80f2a94d212d903a2398094eedc6eb59376
MANISH SINGH <manishsingh332000@gmail.com>
2021-05-23 05:48:21 +0530

    Unfollow the user

--- routes/users.js
+++ routes/users.js
@@ -91,4 +91,32 @@ router.put('/:id/follow', async (req,res) =>{
         res.status(403).json("You can't follow yourself idiot");
     }
 })
+
+
+//Unfollow a user
+router.put('/:id/unfollow', async (req,res) =>{
+    if(req.body.userID !== req.params.id)
+    {
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userID);
+            if(user.followers.includes(req.body.userID))
+            {
+                await user.updateOne({$pull: { followers: req.body.userID }});
+                await currentUser.updateOne({$pull: { following: req.params.id }});
+                res.status(200).json("User has been unfollowed");
+            }
+            else{
+                res.status(403).json("You don't follow this user");
+            }
+        }catch(err)
+        {
+            res.status(500).json(err);
+        }
+    }
+    else{
+        res.status(403).json("You can't unfollow yourself idiot");
+    }
+})
+
 module.exports = router;
