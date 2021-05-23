01fefb65d05b374c864439311ff3563ee3af1b7e
MANISH SINGH <manishsingh332000@gmail.com>
2021-05-23 13:11:11 +0530

    working on front end

--- client/frontend/src/components/top-bar/Topbar.jsx
+++ client/frontend/src/components/top-bar/Topbar.jsx
@@ -1,12 +1,40 @@
 import "./Topbar.css"
-
+import {Search, Person, Chat, Notifications} from '@material-ui/icons'
 function Topbar(props) {
     return (
         <div>
             <h3>TopBar</h3>
-            <div className="topbarLeft"></div>
-            <div className="topbarCenter"></div>
-            <div className="topbarRight"></div>
+            <div className="topbarLeft">
+                <span className="logo">wwe</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchBar">
+                    <Search />
+                    <input placeholder="Search Here......." className="searchInput" />
+                </div>
+            </div>
+                
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">HomePage</span>
+                    <span className="topbarLink">TimeLine</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person />
+                        <span className="topbarIconBatch">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Chat  />
+                        <span className="topbarIconBatch">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications />
+                        <span className="topbarIconBatch">1</span>
+                    </div>
+                </div>
+                <img src="" alt="" className="topbarImage" />
+            </div>
         </div>
     );
 }
