4a7abde281f76cb18d55c8c004230ad56dd964f2
MANISH SINGH <manishsingh332000@gmail.com>
2021-05-24 07:27:43 +0530

    Home page almost completed

--- client/frontend/src/components/post/Post.jsx
+++ client/frontend/src/components/post/Post.jsx
@@ -1,24 +1,25 @@
 import { Favorite, FavoriteBorder, MoreVert, ThumbUpAlt } from '@material-ui/icons';
 import React from 'react';
 import "./post.css"
-
-function Post(props) {
+import { Users } from "../../dummy"
+function Post({post}) {
+    
     return (
         <div className = "post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src="./assets/singer2.jpg" alt="DP" className="postProfileImg" />
-                        <span className="postUserName">John Cena</span>
-                        <span className="postDate">5 mins ago</span>
+                        <img src={Users.filter((u)=>u.id===post?.userId)[0].profilePicture} alt="DP" className="postProfileImg" />
+                        <span className="postUserName">{Users.filter((u)=>u.id===post?.userId)[0].username}</span>
+                        <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">Hey! It's my first post :)</span>
-                    <img src="/assets/post/1.png" alt="post1" className="postImg" />
+                    <span className="postText">{post?.desc}</span>
+                    <img src={post.photo} alt="post1" className="postImg" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
@@ -29,12 +30,12 @@ function Post(props) {
                             <FavoriteBorder />
                         </button>
                         <span className="postLikeCounter">
-                            32 people like it
+                            {post.like}
                         </span>
                     </div>
                     <div className="postBottomRight">
                         <span className="postCommentText">
-                            9 Comments
+                            {post.comment}
                         </span>
                     </div>
                 </div>
