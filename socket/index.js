6ae58f00f85a72e74c1c816c1ef1a5e5e9ab6dca
MANISH SINGH <manishsingh332000@gmail.com>
2021-05-29 12:44:38 +0530

    working on socket chat

---
+++ socket/index.js
@@ -0,0 +1,48 @@
+const io = require('socket.io')(8990, {
+    cors:{
+        origin: "http://localhost:3000"
+    }
+});
+
+let users = []
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+        users.push({userId, socketId})
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+}
+
+const getUser = (userId) => {
+    return users.find(user=> user.userId === userId)
+}
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    //Take userId and socketId for user
+    socket.on("addUser", (userId) => {
+        addUser(userId, socket.id);
+        //Sending data to client 
+        io.emit("getUsers", users);
+    });
+
+    //Event from client use socket.on
+    //Send or get messge from client
+    socket.on("sendMessage", ({senderId, receiverId, text}) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit("getMessage", {
+            senderId,
+            text,
+        })   
+    })
+
+    //On disconnected remove user from user array
+    socket.on("disconnect", () => {
+        console.log("a user disconnected");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    })
+})
+
